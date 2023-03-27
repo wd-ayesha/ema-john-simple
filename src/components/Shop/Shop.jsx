435ac8b24144a190241881f8d8ca78892209027f
Ayesha <ayesha18mbstu@gmail.com>
2023-03-27 21:19:36 +0600

    handled selected item

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,12 +4,20 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect( ()=>{
         fetch('products.json')
         .then(res=>res.json()
         .then(data=>setProducts(data)))
     },[])
+
+    const handleAddToCart = (product) => {
+        // console.log(product)
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart)
+    }
 
     return (
         <>
@@ -20,11 +28,13 @@ const Shop = () => {
                     products.map(product=><Product
                     key={product.id}
                     product= {product}
+                    handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className='cart-container'>
                 <h4>Order Summery</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
         </>
