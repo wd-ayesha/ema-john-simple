56ed88cd5b9403523a4597e13b1f6c0eb13f611c
Ayesha <ayesha18mbstu@gmail.com>
2023-03-27 20:19:31 +0600

    display products

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,14 +4,16 @@ import './Product.css'
 const Product = (props) => {
     const{img, name, price, seller, ratings} = props.product;
     return (
-        <div className='product'>
-           <img src={img} alt="" />
-           <h3>{name}</h3>
-           <p>Price: {price}</p>
-           <p>Manufacturer: {seller}</p>
-           <p>Rating: {ratings} start</p>
-           <button>Add to Cart</button>
-        </div>
+            <div className='product'>
+                <img src={img} alt="" />
+                <div className='product-info'>
+                    <h6 className='product-name'>{name}</h6>
+                    <p>Price: ${price}</p>
+                    <p>Manufacturer: {seller}</p>
+                    <p>Rating: {ratings} stars</p>
+                </div>
+                <button className='btn-cart'>Add to Cart</button>
+            </div>
     );
 };
 
